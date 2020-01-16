7bb9e9c9272b1c74e65fb6b6f5b8f456d18556b6
unknown <2162877@labinfo.is.escuelaing.edu.co>
2020-01-16 15:32:05 -0500

    first commit

---
+++ libreria.py
@@ -0,0 +1,54 @@
+import math
+
+def sumacomplejos(a,b):
+    return (a[0]+b[0],a[1]+b[1])
+    
+def prettyprintingc(c):
+    print(c[0],"+",c[1],"i")
+
+def restacomplejos(a,b):
+    return (a[0]-b[0],a[1]-b[1])
+
+def multicomplejos(a,b):
+    return((a[0]*b[0]-a[1]*b[1]),(a[0]*b[0]+a[1]*b[1]))
+
+def conjugado (a):
+    a = (a[0],(a[1]*-1))
+    return a
+def divcomplejos(a,b):
+    arriba = ((a[0]*a[1]+b[0]*b[1])/((a[1]**2)+(b[1]**2)))
+    abajo = (a[1]*b[0]-a[0]*b[1])/((a[1]**2)+(b[1]**2))
+    fin = (arriba,abajo)
+    return fin
+def modulo(a):
+    modul = (((a[0]**2)+(a[1]**2))**1/2)
+    return modul
+def conversioncap(a):
+    q = math.atan(a[1]/a[0])
+    p = modulo (a)
+    fin = (p,q)
+    return fin
+def fase(a): 
+    return a[1]
+
+
+    
+
+
+a = (-2,1)
+b = (1,2)
+k = sumacomplejos(a,b)
+prettyprintingc(k)
+prettyprintingc(conjugado(b))
+print(divcomplejos(a,b))
+print(modulo(a))
+print (conversioncap(a))
+print (fase(a))
+def main():
+    return
+
+
+
+
+
+
